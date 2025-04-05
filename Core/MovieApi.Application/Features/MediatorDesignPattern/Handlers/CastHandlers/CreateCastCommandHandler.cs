42e7df0ebdf84f3da7dc18b8658fbc8ef05e3334
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-04-05 14:33:46 +0300

    ders 22 sonu

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/CreateCastCommandHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/CreateCastCommandHandler.cs
@@ -13,23 +13,21 @@ namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandl
     public class CreateCastCommandHandler : IRequestHandler<CreateCastCommand>
     {
         private readonly MovieContext _context;
-
         public CreateCastCommandHandler(MovieContext context)
         {
             _context = context;
         }
-
         public async Task Handle(CreateCastCommand request, CancellationToken cancellationToken)
         {
-          _context.Casts.Add(new Cast
-          {
-              Title = request.Title,
-              Name = request.Name,
-              Surname = request.Surname,
-              ImageUrl = request.ImageUrl,
-              Overview = request.Overview,
-              Biography = request.Biography
-          });
+            await _context.Casts.AddAsync(new Cast
+            {
+                Biography = request.Biography,
+                ImageUrl = request.ImageUrl,
+                Name = request.Name,
+                Overview = request.Overview,
+                Surname = request.Surname,
+                Title = request.Title
+            });
             await _context.SaveChangesAsync();
         }
     }
--- Presentation/MovieApi.WebApi/Program.cs
+++ Presentation/MovieApi.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.OpenApi.Models;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+using MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers;
 using MovieApi.Persistence.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,9 @@ builder.Services.AddScoped<GetMovieByIdQueryHandler>();
 builder.Services.AddScoped<CreateMovieCommandHandler>();
 builder.Services.AddScoped<RemoveMovieCommandHandler>();
 builder.Services.AddScoped<UpdateMovieCommandHandler>();
+
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTagQueryHandler).Assembly));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
